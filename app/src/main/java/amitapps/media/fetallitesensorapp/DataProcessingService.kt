6358a65ff790bc2e8c9ebcb2f81640862e33f9b6
amitYadavDev <amit8955461017@gmail.com>
2024-01-18 13:01:27 +0530

    working fine

--- app/src/main/java/amitapps/media/fetallitesensorapp/DataProcessingService.kt
+++ app/src/main/java/amitapps/media/fetallitesensorapp/DataProcessingService.kt
@@ -17,8 +17,13 @@ import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.withContext
+import java.io.BufferedReader
+import java.io.IOException
+import java.io.InputStream
+import java.io.InputStreamReader
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
+import java.util.regex.Pattern
 
 class DataProcessingService : Service() {
 
@@ -41,7 +46,6 @@ class DataProcessingService : Service() {
             val data = readDataFromFile()
 
             // Process and update UI
-            Log.d("MainActivityabcf", "processFileInBg")
             processAndDisplayData(data)
         }
     }
@@ -50,6 +54,58 @@ class DataProcessingService : Service() {
         // Implement logic to read data from the file
         // Return a list of channels, each containing a list of sample values
         // Example: List<List<Double>> = [[channel1], [channel2], [channel3], [channel4]]
+        // Declaring an input stream to read data
+
+//
+//        val data = """"""
+//        val delim = "!"
+
+        val wordsList = mutableListOf<String>()
+        try {
+            val inputStream = resources.openRawResource(R.raw.input)
+            val inputStreamReader = InputStreamReader(inputStream)
+            val bufferedReader = BufferedReader(inputStreamReader)
+
+            var line: String?
+
+            while (bufferedReader.readLine().also { line = it } != null) {
+                var words = line?.split("\\s+".toRegex()) // Split the line into words
+                if (words != null) {
+                    words = words.reversed()
+//                    Log.d("converted_data_size", words.toString() + "   " + words.size.toString())
+
+                    wordsList.addAll(words)
+                }
+            }
+
+            inputStream.close()
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+
+        val channelData = wordsList.map { sample ->
+            Regex("[0-9A-F]{5}").findAll(sample).map { it.value }.toList()
+        }
+
+        // Extract samples and channel data
+//        val samples = Regex("""!(.*?)!""").findAll(data).map { it.groupValues[1] }.toList()
+//        val channelData = list.map { sample ->
+//            Regex("[0-9A-F]{4}").findAll(sample).map { it.value }.toList()
+//        }
+//
+//        // Convert hex to double for each channel
+//        val convertedData = channelData.map { sample ->
+//            sample.map { hexValue ->
+//                Integer.parseInt(hexValue, 16).toDouble() / 1000.0 // Assuming voltage is in millivolts
+//            }
+//        }
+
+        // Print the converted data
+        channelData.forEach { sample ->
+                Log.d("converted_data_", sample.toString())
+        }
+
+
         val listOfLists: List<List<Double>> = listOf(
             listOf(1.0, 2.0, 3.0, 4.0),
             listOf(4.0, 5.0, 6.0, 4.0),
@@ -76,6 +132,7 @@ class DataProcessingService : Service() {
             listOf(4.0, 5.0, 6.0, 4.0),
             listOf(7.0, 8.0, 9.0, 4.0)
         )
+
 
         return listOfLists
     }
